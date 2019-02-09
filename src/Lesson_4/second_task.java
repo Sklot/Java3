61b37fb5141c7dae472fcab37236025898b38076
Denis Ryskal <denis.ryskal@gmail.com>
2019-02-09 18:30:35 +0300

    Lesson_4 task_2

---
+++ src/Lesson_4/second_task.java
@@ -0,0 +1,59 @@
+package Lesson_4;
+
+public class second_task {
+
+    public static void main(String[] args) {
+        get_ready(2000);
+    }
+    static Object mon = new Object();
+
+    static void get_ready(int wfi) {
+
+
+        new Thread(() -> {
+            for (int i = 0; i < 3; i++) {
+
+                try {
+                    synchronized (mon) {
+
+                        Thread.sleep(wfi);
+                        System.out.println("Thread 1");
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+        new Thread(() -> {
+            for (int i = 0; i < 3; i++) {
+
+                try {
+                    synchronized (mon) {
+
+                        Thread.sleep(wfi);
+                        System.out.println("Thread 2");
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+        new Thread(() -> {
+            for (int i = 0; i < 3; i++) {
+
+                try {
+                    synchronized (mon) {
+
+                        Thread.sleep(wfi);
+                        System.out.println("Thread 3");
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+    }
+
+}
